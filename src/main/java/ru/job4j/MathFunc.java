456a78fa80f57cefd412fda608f8d5d260237045
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-07-14 23:12:48 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/MathFunc.java
+++ src/main/java/ru/job4j/MathFunc.java
@@ -1,9 +1,8 @@
 package ru.job4j;
-
 public class MathFunc {
     public static int func1(int x) {
         return x;
-}
+    }
     public static void main(String[] args) {
         int result3= MathFunc.func1(100);
     }
